4a175f0cd3409fcef31617f198c82d5fad038c7c
Fynjy <108663904+GerichkaDrobit@users.noreply.github.com>
2023-10-27 17:37:56 +0500

    Update Ajax_infinity_scroll.js

--- Ajax_infinity_scroll.js
+++ Ajax_infinity_scroll.js
@@ -6,19 +6,32 @@ function loadCustomItems(options) {
     var getURL = options.getURL;
 
     var pageCount = parseInt($((options.paginationSelector) + ' a:last').attr('href').match(/\d+/));
+    
+    
 
-    if (autoLoadButton === true){
-        if (($(options.paginationSelector).length) && (currentPage !== pageEnd || currentPage !== pageCount)) {
+    if (autoLoadButton){
+        if (($(options.paginationSelector).length || !autoLoad) && (currentPage !== pageEnd || currentPage !== pageCount)) {
             $(options.containerSelector).after('<div id="loadMore">Загрузить</div>');
         }
+    }
+    
+    if (autoLoad) {
+        $(window).scroll(function () {
+            if ($(options.containerSelector).scrollTop() + $(options.containerSelector).height() >= $(document).height()) {
+                loadNextPage();
+            }
+        });
     }
 
     function loadNextPage() {
         currentPage++;
         var nextPageURL = getURL + currentPage + '.html';
-        if (currentPage === pageCount || currentPage === pageEnd) {
-            $(options.loadMoreButton).hide();
+        if (!autoLoad) {
+            if (currentPage === pageCount || currentPage === pageEnd) {
+                $(options.loadMoreButton).hide();
+            }
         }
+
         $.ajax({
             url: nextPageURL,
             type: 'GET',
@@ -39,6 +52,7 @@ loadCustomItems({
     paginationSelector: '.pager', //Контейнер с пагинацией
     pageCountElement: '.pager a, .pager span', //Подсчет всех элементов пагинации
     currentPage: '.pager .active', //Определение страницы нахождения
+    autoLoad: false, //True - если хотите чтобы страница загружалась сама без кнопки, false - с кнопки
     autoLoadButton: true, //Вариант работы: true - кнопка добавляется сама, false - кнопка добавляется вручную
     loadMoreButton: '#loadMore', //ID или класс кнопки, делайте че хотите
     getURL: 'page=', //GET параметр пагинации
